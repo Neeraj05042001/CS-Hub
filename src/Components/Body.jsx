412048c1eb8a2fd2dfd9e8a6905b9f5dc390f7c0
Neeraj <neeraj05042001@gmail.com>
2025-02-06 06:04:17 +0530

    junks deleted

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,17 +1,10 @@
-import SimpleResponsiveHeader from "./He";
 import Header from "./Header";
-
-
-import Headers from "./Headers";
 
 const Body = () => {
   return (
     <div>
-      {/* <Header /> */}
-      {/* <div className="font-4xl">Body</div> */}
-
-      {/* <Headers /> */}
-      <SimpleResponsiveHeader/>
+      <Header />
+      <div className="font-4xl">Body</div>
     </div>
   );
 };
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,45 +1,98 @@
 const Header = () => {
   return (
-    <div className="flex justify-between mt-5 mx-[70px]  items-center ">
-      <div className="">
-        <img alt="Logo" src="Logo.svg" />
-      </div>
-      <div className="sm:hidden flex items-center justify-center gap-2.5 w-7 h-7 bg-[#F1F1F1] rounded-[70px]">
-        <img className=" w-3 h-3" alt="search-logo" src="i13mini-search-icon.svg"/>
-      </div>
-      <div className="sm:hidden">
-        <img alt="menu" src="i13mini-menu.svg"/>
-      </div>
-      <div className="hidden sm:block">
-        <div className="bg-[#F1F1F1] w-125 h-13 rounded-[80px] flex  items-center search shadow-[0px_5px_6px_-1px_rgba(0,0,0,0.1)]">
-          <img
-            alt="search"
-            src="search.svg"
-            className="w-[22px] h-[22px] ml-6"
-          />
+    <header className="w-full">
+      <div className="container mx-auto">
+        {/* iPhone 13 mini Layout (default) */}
+        <div className="md:hidden h-7 mt-6 mx-7.5  flex items-center justify-between min-[430px]:hidden ">
+          {/* Logo */}
 
-          <input
-            type="text"
-            placeholder="Search here"
-            className="font-sans text-[16px] font-normal text-[#131313] opacity-60 pl-4 w-full border-none outline-none"
-          />
+          <div className=" W-[269px] h-7 rounded-[46.67px] py-[5.83px] px-2.5 gap-1.5 bg-[#F1F1F1]   flex  items-center shadow-[0px_5px_6px_-1px_rgba(0,0,0,0.1)]">
+            <img
+              alt="search"
+              src="search.svg"
+              className="w-[18px] h-[18px] mx-2"
+            />
+
+            <input
+              type="text"
+              placeholder="Search here"
+              className="font-sans text-[16px] font-normal text-[#131313] opacity-60 pl-4 w-full border-none outline-none -ml-5"
+            />
+          </div>
+          <div className="w-6.5 h-6.5 rounded-[40px] p-2 gap-2.5 bg-[#F1F1F1]">
+            <img alt="cancel-icon" src="cross.svg" />
+          </div>
+        </div>
+
+        {/* iPhone 14 Pro Max Layout */}
+        <div className="@container w-full mt-6 ml-14  ">
+          <div className=" hidden [@media(min-width:430px)_and_(max-width:1023px)]:flex justify-between items-center">
+            {/* Logo */}
+            <div className="w-19.5 h-[30.75px]">
+              <img
+                src="Logo.svg"
+                alt="Logo"
+                className="w-full h-full object-contain"
+              />
+            </div>
+
+            <div className="w-[27.08px] h-[27px] rounded-[70px] p-2 gap-2.5 bg-[#F1F1F1]">
+              <img alt="search-logo" src="i13mini-search-icon.svg" />
+            </div>
+
+            <div className="w-7.5 h-4.5 mr-14">
+              <img alt="menu" src="i13mini-menu.svg" />
+            </div>
+          </div>
+        </div>
+        {/* Desktop/Laptop Layout */}
+        <div className="hidden lg:flex items-center justify-between max-w[1301px] mt-5 mx-17.5">
+          {/* Logo */}
+          <div className="w-[120px] h-[45px]">
+            <img
+              src="Logo.svg"
+              alt="logo"
+              className="w-full h-full object-contain"
+            />
+          </div>
+
+          <div>
+            <div className="bg-[#F1F1F1] w-125 h-13 rounded-[80px] flex  items-center search shadow-[0px_5px_6px_-1px_rgba(0,0,0,0.1)]">
+              <img
+                alt="search"
+                src="search.svg"
+                className="w-[22px] h-[22px] ml-6"
+              />
+
+              <input
+                type="text"
+                placeholder="Search here"
+                className="font-sans text-[16px] font-normal text-[#131313] opacity-60 pl-4 w-full border-none outline-none"
+              />
+            </div>
+          </div>
+
+          <nav className="w-108 h-13 rounded-[80px] bg-[#F1F1F1] py-2.5 px-10 gap-12 cursor-pointer shadow-[0px_5px_6px_-1px_rgba(0,0,0,0.1)] ">
+            <ul className="flex justify-between text-lg text-[#131313] nav-items ">
+              <li className="nav-1  font-bold">Home</li>
+              <li>Olympiad</li>
+              <li>CBSE</li>
+              <li>CUET</li>
+            </ul>
+          </nav>
+
+          <div>
+            <button className=" text-[18px] w-24 h-8 rounded-[50px] py-[3px] px-[15px] bg-[#FBC108] flex items-center cursor-pointer ">
+              Login
+              <span className="pl-2">
+                <img alt="logo" src="login-img.svg" />
+              </span>
+            </button>
+          </div>
         </div>
       </div>
-      <nav className="w-108 h-13 rounded-[80px] bg-[#F1F1F1] py-2.5 px-10 gap-12 cursor-pointer shadow-[0px_5px_6px_-1px_rgba(0,0,0,0.1)] hidden sm:block">
-        <ul className="flex justify-between text-lg text-[#131313] ">
-          <li className="nav-1  font-bold">Home</li>
-          <li>Olympiad</li>
-          <li>CBSE</li>
-          <li>CUET</li>
-        </ul>
-      </nav>
-      
-      <button className=" text-[18px] w-24 h-8 rounded-[50px] py-[3px] px-[15px] bg-[#FBC108] flex items-center hidden sm:block ">
-       Login <span className="pl-2"> <img alt="logo" src="login-img.svg" /> </span>
-      </button>
-    </div>
+    </header>
   );
 };
+
 export default Header;
-
-
